8e10191645ad205154ef225d09886ec3ce4f11a8
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-07-13 12:15:13 +0200

    upgrade to 1.5
    upgrade sqlite requrement too
    
    closes #7

--- conanfile.py
+++ conanfile.py
@@ -4,20 +4,18 @@ import os
 
 class sqlite_ormConan(ConanFile):
     name = "sqlite_orm"
-    version = "1.4"
     description = "SQLite ORM light header only library for modern C++."
     url = "https://github.com/bincrafters/conan-sqlite_orm"
     homepage = "https://github.com/fnc12/sqlite_orm"
     topics = ("conan", "sqlite", "sql", "database", "orm", "sqlite_orm")
     author = "AlexandrePTJ <alpetitjean@gmail.com>"
     license = "BSD 3-Clause"
-    requires = "sqlite3/3.21.0@bincrafters/stable"
+    requires = "sqlite3/3.32.3"
     _source_subfolder = "source_subfolder"
     no_copy_source = True
 
     def source(self):
-        source_url = "https://github.com/fnc12/sqlite_orm"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
